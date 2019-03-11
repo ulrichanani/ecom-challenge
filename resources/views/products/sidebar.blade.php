3ccd77a4e7bec7d25d92a061a470dd5abd26f1f4
Ulrich Anani <ulrichanan@gmail.com>
2019-03-11 03:00:45 +0100

    Review search bar style

--- resources/views/products/sidebar.blade.php
+++ resources/views/products/sidebar.blade.php
@@ -1,127 +1,132 @@
-    <div class="categories_sidebar col-lg-3">
-        <aside class="l_widgest l_p_categories_widget">
-            <div class="l_w_title">
-                <h3>Search</h3>
-            </div>
-            {!! Form::open([
-            'url' => route('search'),
-            'method' => 'get',
-            'class' => 'form-inline'
-            ]) !!}
-            <div class="row">
-
-                <div class="form-group">
-                    {!! Form::text('query', ($query ?? ''), [
-                    'class' => 'form-control',
-                    'placeholder' => "Enter your search",
-                    'required',
-                     'minlength' => 2,
-                    ]) !!}
-                </div>
-                <input type="submit" value="Ok" class="btn btn-primary ml-1">
-                <div class="offset-1 col-10 form-group form-check">
-                    <input type="checkbox" class="form-check-input"
-                           {{ ($searchInBlock ?? '') ? 'checked' : '' }}
-                           name="search_block" id="search_block">
-                    <label class="form-check-label" for="search_block">Search in block</label>
+<div class="categories_sidebar col-md-3">
+    <aside class="l_widgest l_p_categories_widget">
+        <div class="l_w_title">
+            <h3>Search</h3>
+        </div>
+        <div class="row">
+            <div class="col">
+                {!! Form::open([
+                'url' => route('search'),
+                'method' => 'get',
+                'class' => ''
+                ]) !!}
+                <div class="row">
+                    <div class="col-12 form-group">
+                        {!! Form::text('query', ($query ?? ''), [
+                        'class' => 'form-control',
+                        'placeholder' => "Enter your search",
+                        'required',
+                         'minlength' => 2,
+                        ]) !!}
+                    </div>
+                    <div class="offset-1 col form-group form-check">
+                        <input type="checkbox" class="form-check-input"
+                               {{ ($searchInBlock ?? '') ? 'checked' : '' }}
+                               name="search_block" id="search_block">
+                        <label class="form-check-label" for="search_block">Search in block</label>
+                    </div>
+                    <div class="col-md-4">
+                        <input type="submit" value="Ok" class="btn btn-primary ml-1">
+                    </div>
                 </div>
             </div>
             {{ Form::close() }}
-        </aside>
+        </div>
+</aside>
+<aside class="l_widgest l_p_categories_widget">
+    <div class="l_w_title">
+        <h3>Departments</h3>
+    </div>
+    <ul class="navbar-nav">
+        @foreach($DEPARTMENTS as $department)
+            <li class="nav-item">
+                <a class="nav-link"
+                   href="{{ route('departments.show', $department) }}">{{ $department->name }}</a>
+            </li>
+        @endforeach
+    </ul>
+</aside>
+<div class="categories_sidebar">
+    @if(!empty($categories))
         <aside class="l_widgest l_p_categories_widget">
             <div class="l_w_title">
-                <h3>Departments</h3>
+                <h3>{{ $currentDepartment->name }} - Categories</h3>
             </div>
             <ul class="navbar-nav">
-                @foreach($DEPARTMENTS as $department)
+                @foreach($categories as $category)
                     <li class="nav-item">
                         <a class="nav-link"
-                           href="{{ route('departments.show', $department) }}">{{ $department->name }}</a>
+                           href="{{ route('categories.show', $category) }}">{{ $category->name }}</a>
                     </li>
                 @endforeach
             </ul>
         </aside>
-        <div class="categories_sidebar">
-            @if(!empty($categories))
-                <aside class="l_widgest l_p_categories_widget">
-                    <div class="l_w_title">
-                        <h3>{{ $currentDepartment->name }} - Categories</h3>
-                    </div>
-                    <ul class="navbar-nav">
-                        @foreach($categories as $category)
-                            <li class="nav-item">
-                                <a class="nav-link"
-                                   href="{{ route('categories.show', $category) }}">{{ $category->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </aside>
-            @endif
-            {{--<aside class="l_widgest l_fillter_widget">
-                <div class="l_w_title">
-                    <h3>Filter section</h3>
-                </div>
-                <div id="slider-range" class="ui_slider"></div>
-                <label for="amount">Price:</label>
-                <input type="text" id="amount" readonly>
-            </aside>--}}
-            {{--<aside class="l_widgest">
-                <div class="l_w_title">
-                    <h3>Filter price</h3>
-                </div>
-                <table>
-                    <tr>
-                        <th class="pr-2">
-                            <label for="" class="control-label pr-2">Min&nbsp;:&nbsp;</label>
-                        </th>
-                        <td>
-                            <input type="text" class="form-control" id="" value="0">
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="pr-2">
-                            <label for="" class="control-label pr-2">Max&nbsp;:&nbsp;</label>
-                        </th>
-                        <td>
-                            <input type="text" class="form-control" id="" value="0">
-                        </td>
-                    </tr>
-                </table>
-            </aside>
-            @foreach($attributes as $attribute)
-                @if($attribute->values->count() > 0)
-                    <aside class="l_widgest">
-                        <div class="l_w_title">
-                            <h3>{{ $attribute->name }}</h3>
-                        </div>
-                        <ul>
-                            @foreach($attribute->values as $value)
-                                <li><a href="#">{{ $value->value }}</a></li>
-                            @endforeach
-                        </ul>
-                    </aside>
-                @endif
-            @endforeach--}}
-            @if(!empty($featured_products))
-                <aside class="l_widgest l_feature_widget">
-                    <div class="l_w_title">
-                        <h3>Featured Products</h3>
-                    </div>
-                    @foreach($featured_products as $product)
-                        <div class="media">
-                            <div class="d-flex">
-                                <img
-                                    src="{{ asset("/storage/product_images/$product->thumbnail") }}"
-                                    alt="">
-                            </div>
-                            <div class="media-body">
-                                <h4>{{ $product->name }}</h4>
-                                <h5>${{ $product->discounted_price }}</h5>
-                            </div>
-                        </div>
-                    @endforeach
-                </aside>
-            @endif
+    @endif
+    {{--<aside class="l_widgest l_fillter_widget">
+        <div class="l_w_title">
+            <h3>Filter section</h3>
         </div>
-    </div>
+        <div id="slider-range" class="ui_slider"></div>
+        <label for="amount">Price:</label>
+        <input type="text" id="amount" readonly>
+    </aside>--}}
+    {{--<aside class="l_widgest">
+        <div class="l_w_title">
+            <h3>Filter price</h3>
+        </div>
+        <table>
+            <tr>
+                <th class="pr-2">
+                    <label for="" class="control-label pr-2">Min&nbsp;:&nbsp;</label>
+                </th>
+                <td>
+                    <input type="text" class="form-control" id="" value="0">
+                </td>
+            </tr>
+            <tr>
+                <th class="pr-2">
+                    <label for="" class="control-label pr-2">Max&nbsp;:&nbsp;</label>
+                </th>
+                <td>
+                    <input type="text" class="form-control" id="" value="0">
+                </td>
+            </tr>
+        </table>
+    </aside>
+    @foreach($attributes as $attribute)
+        @if($attribute->values->count() > 0)
+            <aside class="l_widgest">
+                <div class="l_w_title">
+                    <h3>{{ $attribute->name }}</h3>
+                </div>
+                <ul>
+                    @foreach($attribute->values as $value)
+                        <li><a href="#">{{ $value->value }}</a></li>
+                    @endforeach
+                </ul>
+            </aside>
+        @endif
+    @endforeach--}}
+    @if(!empty($featured_products))
+        <aside class="l_widgest l_feature_widget">
+            <div class="l_w_title">
+                <h3>Featured Products</h3>
+            </div>
+            @foreach($featured_products as $product)
+                <div class="media">
+                    <div class="d-flex">
+                        <img
+                            src="{{ asset("/storage/product_images/$product->thumbnail") }}"
+                            alt="">
+                    </div>
+                    <div class="media-body">
+                        <h4>{{ $product->name }}</h4>
+                        <h5>${{ $product->discounted_price }}</h5>
+                    </div>
+                </div>
+            @endforeach
+        </aside>
+    @endif
+</div>
+</div>
 </div>
